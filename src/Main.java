ec2a42b6b6b5a9bc3df8caf6b27c48c2b35fd1f7
Lukas <Lukasz90Balwierz@gmail.com>
2022-01-15 07:05:05 +0100

    Zad 4 gotowe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
+
+
+         /* zad.2
+            a) Stworzyć klasę Student z polami prywatnymi
+            - Osoba osoba,
+            - WydzialEnum wydzial;
+            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
+            c) stworzyc konstruktory, gettery i settery
+            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
+         */
+        List<Student> studentList = new ArrayList<>();
+
+        Student student = new Student();
+        student.wydzial = WydzialEnum.Informatyki;
+        student.nazwa = "Patryk";
+        studentList.add(student);
+
+
+        student = new Student();
+        student.nazwa = "Lukasz";
+        student.wydzial = WydzialEnum.Matematyki;
+        studentList.add(student);
+
+
+        for (Student s : studentList) {
+            System.out.println(s.nazwa + " - " +  s.wydzial);
+
+        }
+
+
+    }
+
+
+}
+class Student {
+    String nazwa;
+    WydzialEnum wydzial;
+
+    public String getNazwa(String nazwa) {
+        return nazwa;
+    }
+    public void setImie(String nazwa) {
+        this.nazwa = "Tomasz";
+    }
+
+
+}
+
+
         /* ćw 4 klasy */
 
         /* zad.1
@@ -13,14 +67,45 @@ public class Main {
             c) napisać metodę w klasie Osoba która zwróci informację o osobie,
          */
 
-        /* zad.2
-            a) Stworzyć klasę Student z polami prywatnymi
-            - Osoba osoba,
-            - WydzialEnum wydzial;
-            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
-            c) stworzyc konstruktory, gettery i settery
-            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
-         */
+        class Osoba {
+            private String imie;
+            private String nazwisko;
+            private Integer indeks;
+
+
+            Osoba(String imie, String nazwisko, Integer indeks) {
+
+                this.imie = "Patryk";
+                this.nazwisko = "Nowak";
+                this.indeks = 12345;
+
+            }
+            public String getImie() {
+                return imie;
+            }
+            public String getNazwisko() {
+                return nazwisko;
+            }
+            public int getIndeks() {
+                return indeks;
+            }
+
+            public void setImie(String imie) {
+                this.imie = "Marek";
+            }
+            public void setNazwisko(String nazwisko) {
+                this.nazwisko = "Kowalski";
+            }
+            public void setIndeks(int indeks) {
+                this.indeks = 54321;
+            }
+
+            public void Informacje_o_osobie() {
+                System.out.println("Imie: " + imie + ", Nazwisko: " + nazwisko);
+            }
+        }
+
+
 
         /* zad.3
             a) W klasie Main stworzyć 5 różnych studentów, (można za pomocą tego samego objektu)
@@ -28,5 +113,5 @@ public class Main {
             c) przeiterować listę za pomocą pętli forEach wywołując na każdym objekcie metodę toString() (w każdej iteracji);
          */
 
-    }
-}
+
+
---
+++ src/WydzialEnum.java
@@ -0,0 +1,5 @@
+enum WydzialEnum {
+    Informatyki,
+    Matematyki,
+    Chemii;
+    }
